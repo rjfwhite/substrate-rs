9b941341642a214e0a1d18ee010869a6edd063e6
rob <rob@improbable.io>
2020-03-14 21:58:30 +0000

    finally got a version matrix that works...

--- src/support/mod.rs
+++ src/support/mod.rs
@@ -1,7 +1,4 @@
-use glium::glutin;
-use glium::glutin::event::{Event, WindowEvent};
-use glium::glutin::event_loop::{ControlFlow, EventLoop};
-use glium::glutin::window::WindowBuilder;
+use glium::glutin::{self, Event, WindowEvent};
 use glium::{Display, Surface};
 use imgui::{Context, FontConfig, FontGlyphRanges, FontSource, Ui};
 use imgui_glium_renderer::Renderer;
@@ -11,7 +8,7 @@ use std::time::Instant;
 mod clipboard;
 
 pub struct System {
-    pub event_loop: EventLoop<()>,
+    pub events_loop: glutin::EventsLoop,
     pub display: glium::Display,
     pub imgui: Context,
     pub platform: WinitPlatform,
@@ -24,13 +21,13 @@ pub fn init(title: &str) -> System {
         Some(idx) => title.split_at(idx + 1).1,
         None => title,
     };
-    let event_loop = EventLoop::new();
+    let events_loop = glutin::EventsLoop::new();
     let context = glutin::ContextBuilder::new().with_vsync(true);
-    let builder = WindowBuilder::new()
+    let builder = glutin::WindowBuilder::new()
         .with_title(title.to_owned())
-        .with_inner_size(glutin::dpi::LogicalSize::new(1024f64, 768f64));
+        .with_dimensions(glutin::dpi::LogicalSize::new(1024f64, 768f64));
     let display =
-        Display::new(builder, context, &event_loop).expect("Failed to initialize display");
+        Display::new(builder, context, &events_loop).expect("Failed to initialize display");
 
     let mut imgui = Context::create();
     imgui.set_ini_filename(None);
@@ -45,17 +42,18 @@ pub fn init(title: &str) -> System {
     {
         let gl_window = display.gl_window();
         let window = gl_window.window();
-        platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Rounded);
+        platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Default);
     }
 
     let hidpi_factor = platform.hidpi_factor();
     let font_size = (13.0 * hidpi_factor) as f32;
+
     imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
 
     let renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
 
     System {
-        event_loop,
+        events_loop,
         display,
         imgui,
         platform,
@@ -65,53 +63,49 @@ pub fn init(title: &str) -> System {
 }
 
 impl System {
-    pub fn main_loop<F: FnMut(&mut bool, &mut Ui, &mut glium::Frame, &Display) + 'static>(self, mut run_ui: F) {
+    pub fn main_loop<F: FnMut(&mut bool, &mut Ui, &mut glium::Frame, &Display)>(self, mut run_ui: F) {
         let System {
-            event_loop,
+            mut events_loop,
             display,
             mut imgui,
             mut platform,
             mut renderer,
             ..
         } = self;
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
         let mut last_frame = Instant::now();
+        let mut run = true;
 
-        event_loop.run(move |event, _, control_flow| match event {
-            Event::NewEvents(_) => last_frame = imgui.io_mut().update_delta_time(last_frame),
-            Event::MainEventsCleared => {
-                let gl_window = display.gl_window();
-                platform
-                    .prepare_frame(imgui.io_mut(), &gl_window.window())
-                    .expect("Failed to prepare frame");
-                gl_window.window().request_redraw();
-            }
-            Event::RedrawRequested(_) => {
-                let mut ui = imgui.frame();
+        while run {
+            events_loop.poll_events(|event| {
+                platform.handle_event(imgui.io_mut(), &window, &event);
 
-                let mut run = true;
-                run_ui(&mut run, &mut ui);
-                if !run {
-                    *control_flow = ControlFlow::Exit;
+                if let Event::WindowEvent { event, .. } = event {
+                    if let WindowEvent::CloseRequested = event {
+                        run = false;
+                    }
                 }
+            });
 
-                let gl_window = display.gl_window();
-                let mut target = display.draw();
-                target.clear_color_srgb(1.0, 1.0, 1.0, 1.0);
-                platform.prepare_render(&ui, gl_window.window());
-                let draw_data = ui.render();
-                renderer
-                    .render(&mut target, draw_data)
-                    .expect("Rendering failed");
-                target.finish().expect("Failed to swap buffers");
-            }
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            event => {
-                let gl_window = display.gl_window();
-                platform.handle_event(imgui.io_mut(), gl_window.window(), &event);
-            }
-        })
+            let io = imgui.io_mut();
+            platform
+                .prepare_frame(io, &window)
+                .expect("Failed to start frame");
+            last_frame = io.update_delta_time(last_frame);
+            let mut ui = imgui.frame();
+
+            let mut target = display.draw();
+            run_ui(&mut run, &mut ui, &mut target, &display);
+
+
+            // target.clear_color_srgb(1.0, 1.0, 1.0, 1.0);
+            platform.prepare_render(&ui, &window);
+            let draw_data = ui.render();
+            renderer
+                .render(&mut target, draw_data)
+                .expect("Rendering failed");
+            target.finish().expect("Failed to swap buffers");
+        }
     }
 }
